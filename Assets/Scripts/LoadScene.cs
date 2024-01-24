dbc815b001ee0568ef750e0449b6352eed05d44f
ArnauXeZ <arnauetxpop@get.cat>
2024-01-25 00:23:05 +0100

    MainMenu

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    // Nombre de la escena que quieres cargar
+    public string nombreDeLaEscenaACargar = "NombreDeTuEscena";
+
+    // Método que se llama al presionar el botón de reproducción
+    public void CargarEscena()
+    {
+        // Cargar la escena por su nombre
+        SceneManager.LoadScene(nombreDeLaEscenaACargar);
+    }
+}
